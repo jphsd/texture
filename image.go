4d443614e7b3751007f29a553b5315256820ac3a
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2021-07-02 11:49:40 -0700

    Moved Image2 -> Image

--- image.go
+++ image.go
@@ -1,6 +1,7 @@
 package texture
 
 import (
+	g2dcol "github.com/jphsd/graphics2d/color"
 	"image"
 	"image/color"
 	"image/draw"
@@ -9,46 +10,41 @@ import (
 
 // Image holds the data to support a continuous bicubic interpolation over an image.
 type Image struct {
-	Image         *image.Gray16
+	Image         *image.NRGBA
 	Max           []float64
 	Width, Height int
-	FFunc         func(float64) float64
+	HSL           bool
 }
 
 // NewImage sets up a new field with the supplied image. The image is converted to a {0, 0}
-// offset Gray16 image.
+// offset image.
 func NewImage(img image.Image) *Image {
 	r := img.Bounds()
 	w, h := r.Dx(), r.Dy()
 	nr := image.Rect(0, 0, w, h)
-	gimg := image.NewGray16(nr)
+	gimg := image.NewNRGBA(nr)
 	draw.Draw(gimg, nr, img, r.Min, draw.Src)
-	return &Image{gimg, []float64{float64(w), float64(h)}, w - 1, h - 1, nil}
+	return &Image{gimg, []float64{float64(w), float64(h)}, w - 1, h - 1, false}
 }
 
-// Eval2 returns the value [-1,1] of the field at x, y.
-func (f *Image) Eval2(x, y float64) float64 {
+// Eval2 returns the values [0,1] of the field at x, y.
+func (f *Image) Eval2(x, y float64) []float64 {
 	if x < 0 || x >= f.Max[0] || y < 0 || y >= f.Max[1] {
-		return 0
+		return []float64{0, 0, 0, 1}
 	}
 	nx, ny := math.Floor(x), math.Floor(y)
 	rx, ry := x-nx, y-ny
 	ix, iy := int(nx), int(ny)
 	p := f.getValues(ix, iy)
-	v := 0.0
-	if f.FFunc == nil {
-		v = f.BiCubic(rx, ry, p)
-	} else {
-		v = f.FFunc(f.BiCubic(rx, ry, p))
-	}
-	return v*2 - 1
+	v := BiCubic(rx, ry, p)
+	return v
 }
 
 // Get 4x4 patch
-func (f *Image) getValues(x, y int) [][]float64 {
-	res := make([][]float64, 4)
+func (f *Image) getValues(x, y int) [][][]float64 {
+	res := make([][][]float64, 4)
 	for r, i := y-1, 0; r < y+3; r++ {
-		res[i] = make([]float64, 4)
+		res[i] = make([][]float64, 4)
 		for c, j := x-1, 0; c < x+3; c++ {
 			res[i][j] = f.getValue(c, r)
 			j++
@@ -58,8 +54,8 @@ func (f *Image) getValues(x, y int) [][]float64 {
 	return res
 }
 
-// Get converted gray value and handle edges
-func (f *Image) getValue(x, y int) float64 {
+// Get converted values and handle edges
+func (f *Image) getValue(x, y int) []float64 {
 	var col color.Color
 	if x < 0 {
 		if y < 0 {
@@ -84,15 +80,35 @@ func (f *Image) getValue(x, y int) float64 {
 	} else {
 		col = f.Image.At(x, y)
 	}
-	gc, _ := col.(color.Gray16)
-	v := float64(gc.Y)
-	v /= 0xffff
-	return v
+	if f.HSL {
+		// HSLA
+		hsl := g2dcol.NewHSL(col)
+		return []float64{hsl.H, hsl.S, hsl.L, hsl.A}
+	}
+	// NRGBA
+	c, _ := col.(color.NRGBA)
+	r := uint32(c.R)
+	r = r<<8 | r
+	rv := float64(r)
+	rv /= 0xffff
+	g := uint32(c.G)
+	g = g<<8 | g
+	gv := float64(g)
+	gv /= 0xffff
+	b := uint32(c.B)
+	b = b<<8 | b
+	bv := float64(b)
+	bv /= 0xffff
+	a := uint32(c.A)
+	a = a<<8 | a
+	av := float64(a)
+	av /= 0xffff
+	return []float64{rv, gv, bv, av}
 }
 
 // Cubic calculates the value of f(t) for t in range [0,1] given the values of t at -1, 0, 1, 2 in p[]
 // fitted to a cubic polynomial: f(t) = at^3 + bt^2 + ct + d. Clamped because it over/undershoots.
-func (f *Image) Cubic(t float64, p []float64) float64 {
+func Cubic(t float64, p []float64) float64 {
 	v := p[1] + 0.5*t*(p[2]-p[0]+t*(2.0*p[0]-5.0*p[1]+4.0*p[2]-p[3]+t*(3.0*(p[1]-p[2])+p[3]-p[0])))
 	if v < 0 {
 		v = 0
@@ -103,11 +119,15 @@ func (f *Image) Cubic(t float64, p []float64) float64 {
 }
 
 // BiCubic uses Cubic to calculate the value of f(u,v) for u,v in range [0,1].
-func (f *Image) BiCubic(u, v float64, p [][]float64) float64 {
-	np := make([]float64, 4)
-	np[0] = f.Cubic(v, p[0])
-	np[1] = f.Cubic(v, p[1])
-	np[2] = f.Cubic(v, p[2])
-	np[3] = f.Cubic(v, p[3])
-	return f.Cubic(u, np)
+func BiCubic(u, v float64, p [][][]float64) []float64 {
+	res := make([]float64, 4)
+	for i := 0; i < 4; i++ {
+		np := make([]float64, 4)
+		np[0] = Cubic(v, p[i][0])
+		np[1] = Cubic(v, p[i][1])
+		np[2] = Cubic(v, p[i][2])
+		np[3] = Cubic(v, p[i][3])
+		res[i] = Cubic(u, np)
+	}
+	return res
 }
