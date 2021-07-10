61fae5f93a68b3732dba905ff556ec8e25f277bb
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2021-07-09 17:03:27 -0700

    Added transforms for VectorField and ColorField, added displacement basaed on vetor field

--- displace.go
+++ displace.go
@@ -2,11 +2,27 @@ package texture
 
 type Displace struct {
 	Src, SrcX, SrcY Field
+	OffsX, OffsY    float64
 	ScaleX, ScaleY  float64
 }
 
 func (d *Displace) Eval2(x, y float64) float64 {
-	dvx := d.SrcX.Eval2(x, y) * d.ScaleX
-	dvy := d.SrcY.Eval2(x, y) * d.ScaleY
+	dvx := d.SrcX.Eval2(x+d.OffsX, y+d.OffsY) * d.ScaleX
+	dvy := d.SrcY.Eval2(x+d.OffsX, y+d.OffsY) * d.ScaleY
+	return d.Src.Eval2(dvx, dvy)
+}
+
+type Displace2 struct {
+	Src            Field
+	DistSrc        VectorField
+	SelX, SelY     int
+	OffsX, OffsY   float64
+	ScaleX, ScaleY float64
+}
+
+func (d *Displace2) Eval2(x, y float64) float64 {
+	dv := d.DistSrc.Eval2(x+d.OffsX, y+d.OffsY)
+	dvx := dv[d.SelX] * d.ScaleX
+	dvy := dv[d.SelY] * d.ScaleY
 	return d.Src.Eval2(dvx, dvy)
 }
--- transform.go
+++ transform.go
@@ -1,6 +1,9 @@
 package texture
 
-import g2d "github.com/jphsd/graphics2d"
+import (
+	g2d "github.com/jphsd/graphics2d"
+	"image/color"
+)
 
 type Transform struct {
 	Src Field
@@ -8,6 +11,28 @@ type Transform struct {
 }
 
 func (t *Transform) Eval2(x, y float64) float64 {
+	pt := []float64{x, y}
+	pts := t.Xfm.Apply(pt)
+	return t.Src.Eval2(pts[0][0], pts[0][1])
+}
+
+type TransformVF struct {
+	Src VectorField
+	Xfm *g2d.Aff3
+}
+
+func (t *TransformVF) Eval2(x, y float64) []float64 {
+	pt := []float64{x, y}
+	pts := t.Xfm.Apply(pt)
+	return t.Src.Eval2(pts[0][0], pts[0][1])
+}
+
+type TransformCF struct {
+	Src ColorField
+	Xfm *g2d.Aff3
+}
+
+func (t *TransformCF) Eval2(x, y float64) color.Color {
 	pt := []float64{x, y}
 	pts := t.Xfm.Apply(pt)
 	return t.Src.Eval2(pts[0][0], pts[0][1])
