ae03e2d5596871fa33a6e577d6b766bc2f9d4398
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2022-06-23 20:11:52 -0700

    go fmt changes

--- cmd/random.go
+++ cmd/random.go
@@ -1,8 +1,10 @@
+//go:build ignore
 // +build ignore
 
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	gi "github.com/jphsd/graphics2d/image"
 	"github.com/jphsd/texture"
@@ -20,6 +22,13 @@ func main() {
 		cf := texture.MakeColorField(6, 0)
 		img := texture.NewRGBA(width, height, cf, 0, 0, 1, 1)
 		gi.SaveImage(img, fmt.Sprintf("%06d", cnt))
+
+		// Emit JSON description of cf
+		b, err := json.Marshal(cf)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		fmt.Printf("%v\n", b)
 		cnt++
 	}
 }
