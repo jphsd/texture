f766cae58f0b759cfc7ab45a3328e2c6e1be5e86
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2022-07-08 16:49:51 -0700

    Added short package description

---
+++ random/doc.go
@@ -0,0 +1,21 @@
+/*
+Package random contains functions, MakeXXX, that can be used to create a random texture tree.
+
+The following code creates a random tree up to six nodes deep and uses it to render to an 800x800 image,
+which is then saved as "example.png"
+
+package main
+
+import (
+	"github.com/jphsd/graphics2d/image"
+	"github.com/jphsd/texture"
+	"github.com/jphsd/texture/random"
+)
+
+func main() {
+	cf := random.MakeColorField(6, 0)
+	img := texture.NewRGBA(800, 800, cf, 0, 0, 1, 1)
+	image.SaveImage(img, "example")
+}
+*/
+package random
