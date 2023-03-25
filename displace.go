7cbb8854196905233d4cf42fab920ba2b8e8b29f
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2023-03-24 18:12:43 -0700

    Added Displace2 with xfms for x and y

--- displace.go
+++ displace.go
@@ -2,7 +2,7 @@ package texture
 
 import "github.com/jphsd/graphics2d"
 
-// Displace allows a source field to be evaluated locations determining by an offset and scaling of
+// Displace allows a source field to be evaluated at locations determined by an offset and scaling of
 // the input x, y coordinates taken from other sources. If Indep is true, then the mapped x, y
 // is independent of the original x, y location.
 type Displace struct {
@@ -22,9 +22,34 @@ func NewDisplace(in, dx, dy Field, scale float64) *Displace {
 // Eval2 implements the Field interface.
 func (d *Displace) Eval2(x, y float64) float64 {
 	vp := []float64{d.SrcX.Eval2(x, y), d.SrcY.Eval2(x, y)}
-	p := d.Xfm.Apply(vp)
+	p := d.Xfm.Apply(vp)[0]
 	if !d.Indep {
-		return d.Src.Eval2(x+p[0][0], y+p[0][1])
+		return d.Src.Eval2(x+p[0], y+p[1])
 	}
-	return d.Src.Eval2(p[0][0], p[0][1])
+	return d.Src.Eval2(p[0], p[1])
+}
+
+// Displace2 allows a source field to be evaluated at locations determined by axis independent transforms of
+// the input x, y coordinates taken from other sources. If Indep is true, then the mapped x, y
+// is independent of the original x, y location.
+type Displace2 struct {
+	Name            string
+	Src, SrcX, SrcY Field
+	XfmX, XfmY      *graphics2d.Aff3
+	Indep           bool
+}
+
+// NewDisplace2 creates a new Displace2 instance using the same source for both x and y displacements.
+func NewDisplace2(in, dx, dy Field, xfmx, xfmy *graphics2d.Aff3) *Displace2 {
+	return &Displace2{"Displace2", in, dx, dy, xfmx, xfmy, false}
+}
+
+// Eval2 implements the Field interface.
+func (d *Displace2) Eval2(x, y float64) float64 {
+	px := d.XfmX.Apply([]float64{d.SrcX.Eval2(x, y), 0})[0]
+	py := d.XfmY.Apply([]float64{0, d.SrcY.Eval2(x, y), 0})[0]
+	if !d.Indep {
+		return d.Src.Eval2(x+px[0], y+py[1])
+	}
+	return d.Src.Eval2(px[0], py[1])
 }
