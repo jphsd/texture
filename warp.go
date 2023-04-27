93780dcada9a46e7362a908fd48710aedd1144ee
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2023-04-26 20:48:36 -0700

    Added RadialWarp

--- warp.go
+++ warp.go
@@ -1,6 +1,9 @@
 package texture
 
-import "image/color"
+import (
+	"image/color"
+	"math"
+)
 
 // Warp applies a deformation to the values passed into the Eval2 function.
 type Warp struct {
@@ -30,7 +33,7 @@ func NewWarpVF(src VectorField, wf func(float64, float64) (float64, float64)) *W
 	return &WarpVF{"WarpVF", src, wf}
 }
 
-// Eval2 implements the Field interface.
+// Eval2 implements the VectorField interface.
 func (w *WarpVF) Eval2(x, y float64) []float64 {
 	x, y = w.Func(x, y)
 	return w.Src.Eval2(x, y)
@@ -47,8 +50,24 @@ func NewWarpCF(src ColorField, wf func(float64, float64) (float64, float64)) *Wa
 	return &WarpCF{"WarpCF", src, wf}
 }
 
-// Eval2 implements the Field interface.
+// Eval2 implements the ColorField interface.
 func (w *WarpCF) Eval2(x, y float64) color.Color {
 	x, y = w.Func(x, y)
 	return w.Src.Eval2(x, y)
+}
+
+// RadialWarp performs a scaled warp around Center for use in the above warp types.
+type RadialWarp struct {
+	Center []float64 // Center of warp
+	RScale float64   // Radial scale
+	CScale float64   // Circumference scale
+}
+
+// Eval converts from Euclidean to radial coords.
+func (rw RadialWarp) Eval(x, y float64) (float64, float64) {
+	lx, ly := x-rw.Center[0], y-rw.Center[1]
+	rr := math.Hypot(lx, ly) * rw.RScale
+	rx := rr*math.Atan2(ly, lx)*rw.CScale + rw.Center[0]
+	ry := rr + rw.Center[1]
+	return rx, ry
 }
