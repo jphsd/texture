7d896c8c206e20f73a71c3b6f1a75ba6d7dc334a
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-07-13 15:04:44 -0700

    Doc change

--- doc.go
+++ doc.go
@@ -356,6 +356,8 @@ In the latter case, by using [github.com/jphsd/graphics2d/image.Colorizer].
   - [NewRadialRGBA]
   - [NewConicRGBA]
 
+# Package Examples
+
 [Chequered]: https://pkg.go.dev/github.com/jphsd/texture#hdr-4_1_Chequered__F_
 [Barnsley88]: https://doi.org/10.1016/c2013-0-10335-2
 [Blinn82]: https://dl.acm.org/doi/10.1145/357306.357310
