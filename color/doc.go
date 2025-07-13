fe11eaf09fa5797bb1efa30cee1efef3dd3af3a0
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-07-13 15:42:35 -0700

    Doc change

--- color/doc.go
+++ color/doc.go
@@ -3,8 +3,8 @@ Package color contains types and functions for color management.
 
 A new color type and model that represents colors as float64 tuples [0,1], along with functions:
 
- - [Add]
- - [Prod]
- - [Scale]
+  - [Add]
+  - [Prod]
+  - [Scale]
 */
 package color
