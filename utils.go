470921946f3e0847e5174f6b1de133fb321b6401
Jonathan Hughes <10040216+jphsd@users.noreply.github.com>
2025-12-04 08:57:40 -0800

    Added elliptical gradients

--- utils.go
+++ utils.go
@@ -1,7 +1,7 @@
 package texture
 
 import (
-	"github.com/jphsd/graphics2d"
+	g2d "github.com/jphsd/graphics2d"
 	"github.com/jphsd/graphics2d/image"
 	"image/color"
 	"math"
@@ -19,8 +19,7 @@ func NewLinearGray16(w, h int, p1, p2 []float64, wf *NonLinear, mirror, once boo
 	}
 	nl := NewNLWave([]float64{lambda}, []*NonLinear{wf}, mirror, once)
 	f1 := NewLinearGradient(nl)
-	xfm := graphics2d.NewAff3()
-	xfm.Rotate(-th)
+	xfm := g2d.Rotate(-th)
 	xfm.Translate(-p1[0], -p1[1])
 	f2 := NewTransform(f1, xfm)
 	return NewTextureGray16(w, h, f2, 0, 0, 1, 1, false)
@@ -32,7 +31,21 @@ func NewRadialGray16(w, h int, c []float64, r float64, wf *NonLinear, mirror, on
 	}
 	nl := NewNLWave([]float64{r}, []*NonLinear{wf}, mirror, once)
 	f1 := NewRadialGradient(nl)
-	xfm := graphics2d.NewAff3()
+	xfm := g2d.Translate(-c[0], -c[1])
+	f2 := NewTransform(f1, xfm)
+	return NewTextureGray16(w, h, f2, 0, 0, 1, 1, false)
+}
+
+func NewEllipticalGray16(w, h int, c []float64, rx, ry, th float64, wf *NonLinear, mirror, once bool) *TextureGray16 {
+	if wf == nil {
+		wf = NewNLLinear()
+	}
+	nl := NewNLWave([]float64{rx}, []*NonLinear{wf}, mirror, once)
+	f1 := NewRadialGradient(nl)
+	// Reverse order
+	xfm := g2d.Translate(c[0], c[1])
+	xfm.Rotate(th)
+	xfm.Scale(1, ry/rx)
 	xfm.Translate(-c[0], -c[1])
 	f2 := NewTransform(f1, xfm)
 	return NewTextureGray16(w, h, f2, 0, 0, 1, 1, false)
@@ -44,8 +57,7 @@ func NewConicGray16(w, h int, c []float64, th float64, wf *NonLinear) *TextureGr
 	}
 	nl := NewNLWave([]float64{256}, []*NonLinear{wf}, true, false)
 	f1 := NewConicGradient(nl)
-	xfm := graphics2d.NewAff3()
-	xfm.Rotate(-th)
+	xfm := g2d.Rotate(-th)
 	xfm.Translate(-c[0], -c[1])
 	f2 := NewTransform(f1, xfm)
 	return NewTextureGray16(w, h, f2, 0, 0, 1, 1, false)
@@ -59,6 +71,10 @@ func NewLinearRGBA(w, h int, p1, p2 []float64, c1, c2 color.Color, wf *NonLinear
 
 func NewRadialRGBA(w, h int, c []float64, r float64, c1, c2 color.Color, wf *NonLinear, mirror, once bool) *image.Colorizer {
 	return image.NewColorizer(NewRadialGray16(w, h, c, r, wf, mirror, once), c1, c2, nil, nil, false)
+}
+
+func NewEllipticalRGBA(w, h int, c []float64, rx, ry, th float64, c1, c2 color.Color, wf *NonLinear, mirror, once bool) *image.Colorizer {
+	return image.NewColorizer(NewEllipticalGray16(w, h, c, rx, ry, th, wf, mirror, once), c1, c2, nil, nil, false)
 }
 
 func NewConicRGBA(w, h int, c []float64, th float64, c1, c2 color.Color, wf *NonLinear) *image.Colorizer {
